9b4cdc6adb644b34f342b89ef00aa0db964de1de
Mubeen Qaiser <mubeen.q.m@gmail.com>
2024-04-25 22:52:03 +0500

    use destination + attempt to display path (failed)

--- serial.cpp
+++ serial.cpp
@@ -13,44 +13,49 @@ using namespace std;
 
 #define inf INT32_MAX
 
+struct Node {
+    int distance;
+    vector<int> path;
+};
+
 // Function to find K shortest path lengths
-void findKShortest(vector<vector<pair<int, int> > > &edges, int n, int k, int source)
-{
-    // Vector to store distances
-    vector<vector<int> > dis(n + 1, vector<int>(k, inf));
+void findKShortest(vector<vector<pair<int, int>>> &edges, int n, int k, int source, int destination) {
+    // Vector to store distances and paths
+    vector<vector<Node>> dis(n + 1, vector<Node>(k, {inf, vector<int>()}));
 
     // Initialization of priority queue
-    priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
     pq.push(make_pair(0, source));
-    dis[source][0] = 0;
+    dis[source][0] = {0, {source}};
 
     // while pq has elements
-    while (!pq.empty())
-    {
-        cout << dis.size() << endl;
+    while (!pq.empty()) {
         // Storing the node value
         int u = pq.top().second;
 
         // Storing the distance value
-        int d = (pq.top().first);
+        int d = pq.top().first;
         pq.pop();
-        if (dis[u][k - 1] < d)
-            continue;
-        vector<pair<int, int> > v = edges[u];
+
+        if (dis[u][k - 1].distance < d) continue;
+
+        vector<pair<int, int>> v = edges[u];
 
         // Traversing the adjacency list
-        for (int i = 0; i < v.size(); i++)
-        {
+        for (int i = 0; i < v.size(); i++) {
             int dest = v[i].first;
             int cost = v[i].second;
 
             // Checking for the cost
-            if (d + cost < dis[dest][k - 1])
-            {
-                dis[dest][k - 1] = d + cost;
+            if (d + cost < dis[dest][k - 1].distance) {
+                dis[dest][k - 1].distance = d + cost;
+                dis[dest][k - 1].path = dis[u][k - 1].path;
+                dis[dest][k - 1].path.push_back(dest);
 
                 // Sorting the distances
-                sort(dis[dest].begin(), dis[dest].end());
+                sort(dis[dest].begin(), dis[dest].end(), [](const Node &a, const Node &b) {
+                    return a.distance < b.distance;
+                });
 
                 // Pushing elements to priority queue
                 pq.push(make_pair((d + cost), dest));
@@ -58,11 +63,13 @@ void findKShortest(vector<vector<pair<int, int> > > &edges, int n, int k, int so
         }
     }
 
-
     // Printing K shortest paths
-    for (int i = 0; i < k; i++)
-    {
-        cout << dis[n][i] << " ";
+    for (int i = 0; i < k; i++) {
+        cout << "Path " << i+1 << ": ";
+        for (int j = 0; j < dis[destination][i].path.size(); j++) {
+            cout << dis[destination][i].path[j] << " ";
+        }
+        cout << " Distance: " << dis[destination][i].distance << endl;
     }
 }
 
@@ -159,10 +166,10 @@ void completeEdgesVector(vector<vector<pair<int, int> > > &edges, const string &
 
 int main(int argc, char **argv)
 {
-    string filename = "euemail.txt";
-    int uniqueNodeCount = countUniqueNodes(filename);
-    vector<vector<pair<int, int> > > edges(uniqueNodeCount + 1);
-    completeEdgesVector(edges, filename);
+//    string filename = "euemail.txt";
+//    int uniqueNodeCount = countUniqueNodes(filename);
+//    vector<vector<pair<int, int> > > edges(uniqueNodeCount + 1);
+//    completeEdgesVector(edges, filename);
     clock_t start, end;
     double cpu_time_used;
 
@@ -171,19 +178,28 @@ int main(int argc, char **argv)
     int random_selected_pairs[noOfPairs][2];
 
     // Generate random pairs only in the root process
-    generate_random_pairs(random_selected_pairs, noOfPairs, uniqueNodeCount);
-    print_pairs(random_selected_pairs);
+//    generate_random_pairs(random_selected_pairs, noOfPairs, uniqueNodeCount);
+//    print_pairs(random_selected_pairs);
 
-    cout << "Nodes = " << uniqueNodeCount << endl;
-    cout << "uniqueNodeCount = " << uniqueNodeCount << endl;
-    cout << "edges.size() = " << edges.size() << endl;
+//    cout << "Nodes = " << uniqueNodeCount << endl;
+//    cout << "uniqueNodeCount = " << uniqueNodeCount << endl;
+//    cout << "edges.size() = " << edges.size() << endl;
     cout << "K = " << K << endl;
 
+    vector<vector<pair<int, int> > > edges(6);
+
+    edges[2].push_back(make_pair(3, 2));
+    edges[2].push_back(make_pair(4, 6));
+    edges[3].push_back(make_pair(2, 8));
+    edges[3].push_back(make_pair(4, 1));
+
     start = clock();
-    for (int i = 0; i < noOfPairs; i++)
-    {
-        findKShortest(edges, uniqueNodeCount, K, random_selected_pairs[i][0]);
-    }
+//    for (int i = 0; i < noOfPairs; i++)
+//    {
+//        findKShortest(edges, uniqueNodeCount, K, 1);
+//    }
+    findKShortest(edges, 4, K, 2, 3);
+
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("Time taken for serial: %f seconds\n", cpu_time_used);
